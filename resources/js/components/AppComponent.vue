9f14186dc300a1180fa13af2c112dddba880c921
Idegart <degtyarev_artem@mail.ru>
2019-11-18 23:51:38 +0300

    Init

---
+++ resources/js/components/AppComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        hello!
+    </div>
+</template>
+
+<script>
+    import connect from '@vkontakte/vk-connect';
+
+    export default {
+        name: "AppComponent",
+
+        mounted() {
+            connect.subscribe((e) => {
+                console.log({e})
+            });
+            connect.send("VKWebAppInit", {});
+
+            // connect.sendPromise('VKWebAppGetUserInfo');
+            // connect.send("VKWebAppInit", {});
+            connect.send("VKWebAppGetUserInfo", {});
+
+            connect.sendPromise('VKWebAppGetUserInfo');
+
+            // connect.send("VKWebAppCallAPIMethod", {"method": "execute.ИМЯ_ПРОЦЕДУРЫ", "params": {"НАЗВАНИЕ_ПАРАМЕТРА": "ЗНАЧЕНИЕ_ПАРАМЕТРА", "v": "ВЕРСИЯ_API", "access_token": "ВАШ_КЛЮЧ_ДОСТУПА"}});
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
