47fa0788e2f854c5815a970de2f54006f4bdc44f
Idegart <degtyarev_artem@mail.ru>
2019-11-19 00:51:58 +0300

    Test

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -10,23 +10,47 @@
     export default {
         name: "AppComponent",
 
+        data: () => ({
+            app_id: 7213797,
+            user_id: null,
+            access_token: null,
+        }),
+
+        methods: {
+            loadUsers () {
+                if (!this.user_id || !this.access_token) {
+                    return
+                }
+
+                connect.send("VKWebAppCallAPIMethod", {
+                    "method": "friends.get",
+                    "request_id": "32test",
+                    "params": {
+                        "user_id": this.user_id,
+                        "order": "hints",
+                        "access_token": this.access_token,
+                    }
+                });
+            },
+        },
+
         mounted() {
             connect.subscribe((e) => {
                 console.log({e})
+                
+                if (e.detail.type === "VKWebAppAccessTokenReceived") {
+                    this.loadUsers()
+                }
+
+                if (e.detail.type === "VKWebAppGetUserInfoResult") {
+                    this.loadUsers()
+                }
             });
             connect.send("VKWebAppInit", {});
 
-            // connect.sendPromise('VKWebAppGetUserInfo');
-            // connect.send("VKWebAppInit", {});
             connect.send("VKWebAppGetUserInfo", {});
 
-            connect.send("VKWebAppGetAuthToken", {"app_id": 7213797, "scope": "friends,status"});
-
-            connect.send('VKWebAppGetFriends', {});
-
-            console.log('test')
-
-            // connect.send("VKWebAppCallAPIMethod", {"method": "execute.ИМЯ_ПРОЦЕДУРЫ", "params": {"НАЗВАНИЕ_ПАРАМЕТРА": "ЗНАЧЕНИЕ_ПАРАМЕТРА", "v": "ВЕРСИЯ_API", "access_token": "ВАШ_КЛЮЧ_ДОСТУПА"}});
+            connect.send("VKWebAppGetAuthToken", {"app_id": this.app_id, "scope": "friends,status"});
         }
     }
 </script>
