482d5d667a31ee9dc6d4389725ef216ff0c350e4
Idegart <degtyarev_artem@mail.ru>
2019-11-19 13:52:57 +0300

    Test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta http-equiv="Pragma" content="no-cache" />
+
     <title>{{ config('app.name') }}</title>
 
     <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
