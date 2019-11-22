bda5ef48d1181b07b26e180d38dfce26d4cb958c
Idegart <degtyarev_artem@mail.ru>
2019-11-22 11:45:07 +0300

    test

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -4,34 +4,37 @@
 </template>
 
 <script>
-    import {mapActions, mapState} from 'vuex'
+    // import {mapActions, mapState} from 'vuex'
+    //
+    // import connect from '@vkontakte/vk-connect';
 
-    import connect from '@vkontakte/vk-connect';
+    import Game from "../plugins/Game";
 
     export default {
         name: "AppComponent",
 
         computed: {
-            ...mapState({
-                user: 'user/user',
-            }),
+            // ...mapState({
+            //     user: 'user/user',
+            // }),
         },
 
         methods: {
-            ...mapActions({
-                initUserApp: 'user/initApp',
-                handleEvent: 'user/handleEvent',
-                initBaseApp: 'app/initApp',
-            }),
+            // ...mapActions({
+            //     initUserApp: 'user/initApp',
+            //     handleEvent: 'user/handleEvent',
+            //     initBaseApp: 'app/initApp',
+            // }),
         },
 
         mounted() {
-            connect.subscribe((e) => {
-                this.handleEvent(e)
-            });
-
-            this.initUserApp({app_id: 7213797})
-            this.initBaseApp()
+            let game = new Game(document.getElementById('app-container'))
+            // connect.subscribe((e) => {
+            //     this.handleEvent(e)
+            // });
+            //
+            // this.initUserApp({app_id: 7213797})
+            // this.initBaseApp()
         }
     }
 </script>
---
+++ resources/js/plugins/Game.js
@@ -0,0 +1,42 @@
+import * as PIXI from "pixi.js";
+import nipplejs from "nipplejs";
+
+
+export default class Game {
+    constructor (rootEl) {
+        this.rootEl = rootEl;
+        this.rootApp = this._setRootApp();
+
+        this._setJoystick()
+    }
+
+    _setRootApp() {
+        let app = new PIXI.Application({
+                width: window.innerWidth,
+                height: window.innerHeight,
+                // antialias: true,
+                transparent: false,
+                // resolution: 1
+            }
+        )
+
+        app.renderer.backgroundColor = 0x061639;
+
+        this.rootEl.appendChild(app.view);
+
+        app.loader
+            .add("https://sun9-23.userapi.com/c623422/v623422145/34dbc/2BLzJkwdTMg.jpg?ava=1")
+
+        return app
+    }
+
+    _setJoystick () {
+        let joystick = nipplejs.create({
+            zone: this.rootEl,
+            mode: 'static',
+            position: {right: '50%', bottom: '15%'},
+            dynamicPage: true,
+        });
+    }
+
+}
