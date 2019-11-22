2ddfc432add75f2a28c581b0c3ce3899ae017598
Idegart <degtyarev_artem@mail.ru>
2019-11-22 14:10:48 +0300

    test

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -2,7 +2,7 @@
     <div id="app-container">
 
         <div id="scoreModal" class="modal" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
+            <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Поздравляем!</h5>
