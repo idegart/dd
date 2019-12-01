8103126b07355280d45bc65ccdfe64d8479a2f78
Idegart <degtyarev_artem@mail.ru>
2019-12-01 21:34:02 +0300

    test

--- resources/js/games/SocialGame/SocialGame.js
+++ resources/js/games/SocialGame/SocialGame.js
@@ -56,7 +56,6 @@ export default class SocialGame extends BaseGameScene {
         this.load.image('bridge', 'bridge.png');
         this.load.image('ball', 'ball.png');
         this.load.image('forest', 'forest.png');
-        this.load.image('hole', 'hole.png');
         this.load.image('construction', 'construction.png');
         this.load.image('duma', 'duma.png');
         this.load.image('trash', 'trash1.png');
@@ -240,13 +239,12 @@ export default class SocialGame extends BaseGameScene {
     createEnemy () {
         let enemy;
 
-        switch (random(0, 12)) {
+        switch (random(0, 11)) {
             case 0: enemy = this.createEnemyPoliceman();enemy.name = 'policeman';break;
             case 1: enemy = this.physics.add.sprite(0, 0, 'dollar');enemy.name = 'dollar';break;
             case 2: enemy = this.physics.add.sprite(0, 0, 'bridge');enemy.name = 'bridge';break;
             case 3: enemy = this.physics.add.sprite(0, 0, 'ball');enemy.name = 'ball';break;
             case 4: enemy = this.physics.add.sprite(0, 0, 'forest');enemy.name = 'forest';break;
-            case 5: enemy = this.physics.add.sprite(0, 0, 'hole');enemy.name = 'hole';break;
             case 6: enemy = this.physics.add.sprite(0, 0, 'construction');enemy.name = 'construction';break;
             case 7: enemy = this.physics.add.sprite(0, 0, 'duma');enemy.name = 'duma';break;
             case 8: enemy = this.physics.add.sprite(0, 0, 'trash');enemy.name = 'trash';break;
@@ -408,79 +406,76 @@ export default class SocialGame extends BaseGameScene {
         switch (enemyName) {
             case 'dollar':
                 obj.name = 'Коррупция';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://www.dw.com/image/36703313_303.jpg';
+                obj.description = 'Аудит выявил в Роскосмосе признаки мошенничества и коррупции. Преступные схемы в корпорации, оказывается, «работали десятилетиями».\n' +
+                    'По итогам проверки было возбуждено более 15 уголовных дел';
+                obj.source = 'https://tjournal.ru/news/128612-v-roskosmose-nashli-priznaki-korrupcii-i-moshennichestva-vozbuzhdeno-bolee-15-ugolovnyh-del';
                 obj.target = '';
                 break;
             case 'bridge':
                 obj.name = 'Крымский мост';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://i.obozrevatel.com/gallery/2019/7/18/photo2019-07-1814-46-10.jpg';
+                obj.description = '«Ведомости»: Власти планируют строить новую трассу из Краснодара к Крымскому мосту.\n' +
+                    'Один её километр обойдётся в 1 000 000 000 рублей';
+                obj.source = 'https://meduza.io/news/2019/08/30/ot-krasnodara-k-krymskomu-mostu-planiruyut-postroit-novuyu-trassu-ona-oboydetsya-v-odin-milliard-rubley-za-kilometr?utm_campaign=vedomosti-vlasti-planiruyut-stroit-no';
                 obj.target = '';
                 break;
             case 'ball':
                 obj.name = 'Кокорин и Мамаев';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://cdni.rt.com/russian/images/2018.10/article/5bc347b71835613c338b45b8.jpg';
+                obj.description = 'Суд рассмотрел прошение Александра Протасовицкого об УДО. Это последний фигурант дела Кокорина и Мамаева — друг футболистов, который участвовал в потасовке и сейчас находится в колонии.';
+                obj.source = 'https://sport24.ru/news/football/2019-10-01-sud-otkazalsya-dosrochno-vypuskat-druga-kokorina-i-mamayeva-na-svobodu';
                 obj.target = '';
                 break;
             case 'forest':
                 obj.name = 'Вырубка лесов';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
-                obj.target = '';
-                break;
-            case 'hole':
-                obj.name = 'Дыры на дорогах';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://ecoportal.info/wp-content/uploads/2016/12/vyrubka-lesov-foto1-544x347.jpg';
+                obj.description = 'Россия может временно ограничить экспорт древесины в Китай из-за проблем лесовосстановления и «черных вырубок»';
+                obj.source = 'https://tass.ru/ekonomika/6095475';
                 obj.target = '';
                 break;
             case 'construction':
-                obj.name = 'Долгострои';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.name = 'Обманутые дольщики';
+                obj.image = 'https://politika09.com/wp-content/uploads/2019/10/img_0383.jpg';
+                obj.description = 'В Москве задержали мужчину, облившего себя бензином у здания Минстроя.\n' +
+                    'Он назвал себя обманутым дольщиком и требовал встречи с руководством ведомства';
+                obj.source = 'https://meduza.io/news/2019/06/26/u-zdaniya-minstroya-v-moskve-muzhchina-oblil-sebya-benzinom-on-nazval-sebya-obmanutym-dolschikom-i-potreboval-vstrechi-s-rukovodstvom';
                 obj.target = '';
                 break;
             case 'duma':
                 obj.name = 'Выборы в Думу';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://cdn.vdmsti.ru/crop/image/2019/76/1dg3bs/original-1s30.jpg?height=698&width=1240';
+                obj.description = 'Победившие на выборах в Мосгордуму оппозиционеры потратили на избирательные кампании в 9 раз меньше денег, чем проигравшие провластные кандидаты';
+                obj.source = 'https://www.vedomosti.ru/politics/articles/2019/09/15/811290-proshedshie-mosgordumu-oppozitsioneri';
                 obj.target = '';
                 break;
             case 'trash':
                 obj.name = 'Мусорная реформа';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://gdb.rferl.org/9AC78BBE-D6A3-4C82-9894-A79A807F36F2_w1597_n_r1_st.jpg';
+                obj.description = 'Учёные установили, что проект «мусорной реформы», одобренной президентом, нанесёт огромный ущерб здоровью россиян.';
+                obj.source = 'https://www.svoboda.org/a/29815326.html';
                 obj.target = '';
                 break;
             case 'med':
                 obj.name = 'Уволнение мед.работников';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://images11.esquire.ru/upload/img_cache/b39/b396f5fe9048beb54deb7e13c78e0ac1_ce_2468x1539x0x79_cropped_960x600.jpg';
+                obj.description = 'Сотрудники НИИ детской онкологии и гематологии НМИЦ записали видеообращение, в котором они рассказали общественности о конфликте с руководством института.\n';
+                obj.source = 'https://meduza.io/feature/2019/10/01/vrachi-instituta-detskoy-onkologii-nmits-imeni-blohina-konfliktuyut-s-direktorom';
                 obj.target = '';
                 break;
             case 'policeman':
                 obj.name = 'Полицейский беспредел';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Ivan_Golunov_at_the_Nikulinsky_Districtional_Court%2C_2019-06-08_%283%29.jpg/1920px-Ivan_Golunov_at_the_Nikulinsky_Districtional_Court%2C_2019-06-08_%283%29.jpg';
+                obj.description = 'В Москве полиция задержала спецкорреспондента «Медузы» Ивана Голунова, известного своими расследованиями о коррупции в московской мэрии и воровстве среди депутатов.';
+                obj.source = 'https://ru.wikipedia.org/wiki/%D0%94%D0%B5%D0%BB%D0%BE_%D0%98%D0%B2%D0%B0%D0%BD%D0%B0_%D0%93%D0%BE%D0%BB%D1%83%D0%BD%D0%BE%D0%B2%D0%B0';
                 obj.target = '';
                 break;
             case 'fire':
                 obj.name = '"Экономически невыгодные" пожары';
-                obj.image = '';
-                obj.description = '';
-                obj.source = '';
+                obj.image = 'https://www.ridus.ru/images/2019/7/31/953135/main_slider_f996d82634.jpg';
+                obj.description = 'Весь июль в Сибири горят леса. На данный момент, по официальным данным, площадь горящего леса достигла 1 300 000 га.\n' +
+                    'Однако региональные власти уверяют, что всё под контролем и паниковать не стоит. А большую часть пожаров просто не трогают, говорят, что тушить их экономически невыгодно';
+                obj.source = 'https://secretmag.ru/news/ekonomicheski-nevygodnye-rukovoditeli-gubernatorov-khotyat-otstranit-ot-tusheniya-lesnykh-pozharov-16-08-2019.htm';
                 obj.target = '';
                 break;
         }
